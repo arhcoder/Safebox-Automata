fe7f65a3badf3b376b28e8c4aa5222d8d508429b
Alejandro Ramos <arhcoder@gmail.com>
2022-12-04 15:38:03 -0600

    Safebox Automaton v.1.2.0
    Interface apparently completed, it has a cat; a pretty cat, I no need nothing more...

--- app/scripts/safebox.js
+++ app/scripts/safebox.js
@@ -1,6 +1,11 @@
 // Variables globables:
 var password = "";
-var output = "";
+var output = "ㅤ";
+var clicksnd = new Audio("sound/move.mp3");
+var opensnd = new Audio("sound/open.mp3");
+var closesnd = new Audio("sound/close.mp3");
+var fanfaresnd = new Audio("sound/fanfare.mp3");
+var purrsnd = new Audio("sound/purr.mp3");
 
 function refresh()
 {
@@ -16,7 +21,7 @@ function refresh()
     for (let digit = 1; digit <= 4; digit++)
     {
         // Genera la cantidad de movimientos:
-        let movements = Math.floor(Math.random() * 5) + 1;
+        let movements = Math.floor(Math.random() * 6) + 1;
         for (let movement = 1; movement <= movements; movement++)
         {
             password += currentCharacter;
@@ -32,7 +37,11 @@ function refresh()
     document.getElementById("password-text").innerHTML = password;   
 
     // Refresca la barra de output:
-    document.getElementById("output-text").innerHTML = output;
+    document.getElementById("output-text").innerHTML = "ㅤ";
+
+    // Cambia el svg a la caja cerrada:
+    this.document.querySelector("img[name=box]").src = "img/box-closed.svg";
+    closesnd.play();
 }
 
 function moveLeft()
@@ -51,8 +60,12 @@ function moveRight()
 function reset()
 {
     // Borra la barra de output:
-    output = "";
+    output = "ㅤ";
     document.getElementById("output-text").innerHTML = output;
+
+    // Cambia el svg a la caja cerrada:
+    document.querySelector("img[name=box]").src = "img/box-closed.svg";
+    closesnd.play();
 }
 
 
@@ -89,6 +102,7 @@ window.addEventListener("keyup", function (eventkey)
         var button = this.document.getElementById("left-button");
         button.classList.remove("active-left");
         button.classList.remove("active-press");
+        clicksnd.play();
         moveLeft();
     }
     else if (eventkey.code === "ArrowRight" || eventkey.code === "KeyR")
@@ -96,13 +110,44 @@ window.addEventListener("keyup", function (eventkey)
         var button = this.document.getElementById("right-button");
         button.classList.remove("active-right");
         button.classList.remove("active-press");
+        clicksnd.play();
         moveRight();
     }
     else if (eventkey.code === "Enter" || eventkey.code === "Space")
     {
-        var button = this.document.getElementById("reset-button-id");
-        // button.classList.remove("active-reset-press");
-        // button.classList.add("reset-button:not(:active)");
         reset();
     }
+    else if (eventkey.code == "KeyA")
+    {
+        open();
+    }
 });
+
+function open()
+{
+    // Cambia el svg a la caja abierta:
+    this.document.querySelector("img[name=box]").src = "img/box-opened.svg";
+
+    // Reproduce los sonidos de victoria:
+    opensnd.play();
+    fanfaresnd.play();
+    purrsnd.play();
+
+    // Muestra la ventana con el gato:
+    console.log(jQuery("#victory").modal("show"));
+}
+
+function closeok()
+{
+    // Pausa la fanfarria:
+    fanfaresnd.pause();
+    fanfaresnd.currentTime = 0;
+}
+
+function meow()
+{
+    // Selecciona aleatoriamente UN miau de CINCO para reproducir:
+    let meow = Math.floor(Math.random() * 5) + 1;
+    meowsnd = new Audio("sound/meow"+meow+".mp3");
+    meowsnd.play();
+}
